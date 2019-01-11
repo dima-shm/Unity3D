605c0b971c57a25f1564927d7892f72d8ff7792f
Dima Shumanski <dima.shumanski@gmail.com>
2019-01-11 22:33:09 +0300

    Added Lab_11 — Interacting with objects in a scene

---
+++ Lab_11/Assets/Script.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour {
+
+    private Transform cameraTransform;
+
+    //for applyForce
+    public int force = 2000;
+
+    //movement speed
+    public float speed = 10;
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    void Update()
+    {
+        applyForce();
+        moveKeys();
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+     public void moveKeys()
+     {
+         if(Input.GetKey(KeyCode.DownArrow))
+         {
+            transform.position += cameraTransform.forward * (Time.deltaTime * speed) * -1;
+         }
+         if(Input.GetKey(KeyCode.UpArrow))
+         {
+            transform.position += cameraTransform.forward * (Time.deltaTime * speed);
+         }
+     }
+
+     public void applyForce(){
+
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)){
+ 
+                if (hit.rigidbody)
+                hit.rigidbody.AddForceAtPosition(force * transform.forward, hit.point);
+            }
+        }
+     }   
+}
